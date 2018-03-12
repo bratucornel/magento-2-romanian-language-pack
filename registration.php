61ebbc877a8add56db7c6176ee89dee997776d00
Cornel Bratu <bratucornel@gmail.com>
2018-03-12 14:11:30 +0200

    :zap:

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Magento 2 Romanian Language Pack
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'bratucornel_ro_ro',
+    __DIR__
+);
